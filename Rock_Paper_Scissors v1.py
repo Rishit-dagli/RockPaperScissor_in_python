5a6f5a9bf2d2c20e4aae7aef2138f223f7ac7f58
Rishit-dagli <39672672+Rishit-dagli@users.noreply.github.com>
2018-05-30 21:42:28 +0530

    the v1 of the game
    no looping used will add that too

---
+++ Rock_Paper_Scissors v1.py
@@ -0,0 +1,59 @@
+from random import randint
+
+
+player = input("Player, make your move: ").lower()
+
+rand_num = randint(0,2)
+
+if rand_num == 0:
+
+	computer = "rock"
+
+elif rand_num == 1:
+
+	computer = "paper"
+
+else:
+
+	computer = "scissors"
+
+
+print(f"Computer plays {computer}" )
+
+
+if player == computer:
+
+	print("It's a tie!")
+
+elif player == "rock":
+
+	if computer == "scissors":
+
+		print("player wins!")
+
+	else:
+
+		print("computer wins!")
+
+elif player == "paper":
+
+	if computer == "rock":
+
+		print("player wins!")
+
+	else:
+
+		print("computer wins!")
+
+elif player == "scissors":
+
+	if computer == "paper":
+
+		print("player wins!")
+
+	else:
+
+		print("computer wins!")
+else:
+
+		print("Please enter a valid move!")
